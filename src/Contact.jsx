448c607a10b037ddcded772b891c00b465329e0d
meet2aman <meet2amankushwaha@gmail.com>
2024-03-05 21:08:22 +0530

    enabled contact form sending

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,19 +1,49 @@
 import React, { useState } from "react";
 import Navbar from "./components/Navbar";
-
+import { useForm, ValidationError } from "@formspree/react";
+import { Link } from "react-router-dom";
 const Contact = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [state, handleSubmit] = useForm("xpznzygv");
+  if (state.succeeded) {
+    return (
+      <div className="">
+        <div className="h-45% py-[40px] md:px-[60px] px-[50px]">
+          <h1 className="md:text-[190px] text-[160px] leading-[140px] md:leading-[144px] font-[600] tracking-tight">
+            Hey{" "}
+            <span className="capatalize">
+              {name} <span>🤗</span>
+            </span>
+            <br />
+            <span className="text-[130px] !leading-[50px]">
+              Your Request has been sent successfully
+            </span>
+          </h1>
+          <div className="w-full flex justify-between pt-4 items-center text-[32px] font-[500]">
+            <h1 className="mt-4">({email})</h1>
+            <div className="cursor-pointer md:block flex justify-between items-center">
+              <Link to={"/"}>
+                <button className="py-2 px-4 tracking-wide text-white rounded-lg text-2xl bg-black hover:text-black hover:bg-white shadow-md">
+                  Home
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log({
-      name,
-      email,
-      message,
-    });
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   console.log({
+  //     name,
+  //     email,
+  //     message,
+  //   });
+  // };
 
   return (
     <>
@@ -36,31 +66,49 @@ const Contact = () => {
             className="flex flex-col gap-6 md:gap-6 w-full justify-start items-center my-10 lg:my-20"
           >
             <input
-              type="text"
+              id="name"
+              type="name"
+              name="name"
               onChange={(e) => setName(e.target.value)}
               value={name}
               placeholder="Enter Your Name"
               className="p-4 bg-slate-200 text-black w-full text-lg tracking-wide border-2 border-black"
             />
             <input
+              id="email"
               type="email"
+              name="email"
               onChange={(e) => setEmail(e.target.value)}
               value={email}
               placeholder="Enter Your Email"
               className="p-4 bg-slate-200 text-black w-full  text-lg tracking-wide border-2 border-black"
             />
+            <ValidationError
+              prefix="Email"
+              field="email"
+              errors={state.errors}
+            />
 
             <textarea
               className="w-full text-black bg-slate-200 p-4 text-lg tracking-wide border-2 border-black"
-              name="Enter Your Message"
               placeholder="Enter your message"
-              id=""
+              id="message"
+              name="message"
               cols="30"
               rows="10"
               onChange={(e) => setMessage(e.target.value)}
               value={message}
-            ></textarea>
-            <button className="py-2 px-4 tracking-wide text-white rounded-lg text-2xl bg-black hover:text-black hover:bg-white shadow-md">
+            />
+            <ValidationError
+              prefix="Message"
+              field="message"
+              errors={state.errors}
+            />
+            <button
+              type="submit"
+              disabled={state.submitting}
+              className="py-2 px-4 tracking-wide text-white rounded-lg text-2xl bg-black hover:text-black hover:bg-white shadow-md"
+            >
               Send Message
             </button>
           </form>
